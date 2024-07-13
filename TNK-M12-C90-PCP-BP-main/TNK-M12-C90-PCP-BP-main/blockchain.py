8f7ab63249054985c31c8b20b2e930366c7411fd
adityaDpSuvarna <aditya.dp.suvarna@gmail.com>
2024-07-13 12:57:02 +0530

    Add files via upload

---
+++ TNK-M12-C90-PCP-BP-main/TNK-M12-C90-PCP-BP-main/blockchain.py
@@ -0,0 +1,78 @@
+import hashlib
+import json
+from time import time
+
+def generateHash(input_string):
+    hashObject = hashlib.sha256()
+    hashObject.update(input_string.encode('utf-8'))
+    hashValue = hashObject.hexdigest()
+    return hashValue
+
+class BlockChain():
+    def __init__(self):
+        self.chain = []
+
+    def length(self):
+        return len(self.chain)
+        
+    def addBlock(self, currentBlock):
+        if(len(self.chain) == 0):
+            self.createGensisBlock()
+        currentBlock.previousHash = self.chain[-1].currentHash
+        currentBlock.currentHash = currentBlock.calculateHash()
+        self.chain.append(currentBlock)
+    
+    def createGensisBlock(self):
+        genesisBlock = Block(0, time(), "No Previous Hash.")
+        self.chain.append(genesisBlock)
+    
+    def printChain(self):
+        for block in self.chain:
+            print("Block Index", block.index)
+            print("Timestamp", block.timestamp)
+            print("Transactions", block.transactions)
+            print( "Previous Hash",block.previousHash)
+            print( "Current Hash",block.currentHash)
+            print( "Is Valid Block",block.isValid)
+
+            print("*" * 100 , "\n")
+
+    def validateBlock(self, currentBlock):
+        previousBlock = self.chain[currentBlock.index - 1]
+        if(currentBlock.index != previousBlock.index + 1):
+            return False
+        
+        previousBlockHash = previousBlock.calculateHash()
+        
+        if(previousBlockHash != currentBlock.previousHash):
+            return False
+        
+        return True
+        
+class Block:
+    def __init__(self, index, timestamp, previousHash):
+        self.index = index
+        self.transactions = []
+        self.timestamp = timestamp
+        self.previousHash = previousHash
+        self.currentHash = self.calculateHash()
+        self.isValid = None
+       
+    def calculateHash(self, timestamp=None):
+        if(timestamp == None):
+            timestamp = self.timestamp
+        
+        # Convert all the decimal data of a transaction into string using json.dumps() and store it in a variable .
+        blockString = str(self.index) + str(timestamp) + str(self.previousHash) + json.dumps(self.transactions, default=str)
+        return generateHash(blockString)
+
+    def addTransaction(self, transaction):
+        if transaction:
+            self.transactions.append(transaction)
+            if len(self.transactions) == 3:
+                self.currentHash = self.calculateHash()
+                return "Ready"
+            return "Add more transactions"
+
+
+       
